4c6c1fdf9dd8a8a504ec3344a4e81a316c981abd
Pratibha Ratnam <pbratnam@Pratibhas-MacBook-Pro.local>
2020-01-14 22:57:06 -0500

    base project without node code

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = (env, argv) =>({
+    entry: './src/index.js',
+    output: {
+        path:`${__dirname}/public`,
+        filename: 'bundle.js'
+    },
+    module:{
+        rules: [
+            {
+                test: /\.jsx?/,
+                exclude: /node_modules/,
+                loader: [{
+                loader: 'babel-loader',
+                query: {
+                    presets: ["@babel/preset-react"]
+                }
+            }
+            ]
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+              },
+        ]
+    },
+    devtool: argv.mode ==="development" ? 'source-map' : false
+})
